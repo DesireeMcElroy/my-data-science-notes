62b7d0b35b4dabe4d342eb0ae833085239d32a47
Desiree_McElroy <desireeashley90@gmail.com>
2021-04-03 22:23:42 -0700

    Adding temporary tables exercise notes

--- SQL_Notes.sql
+++ SQL_Notes.sql
@@ -98,4 +98,271 @@ SELECT
 
 
 
+-- SUBQUERIES notes **************************************
 
+USE join_example_db;
+SELECT *
+FROM users;
+
+SELECT * FROM roles;
+
+-- show users that have the role of reviewer
+SELECT *
+FROM users
+WHERE role_id IN(
+	SELECT id
+	FROM roles
+	WHERE `name` = 'reviewer'
+	);
+	
+	
+	
+USE chipotle;
+
+SELECT *
+FROM orders
+LIMIT 5;
+
+
+SELECT *
+FROM orders
+WHERE item_name IN(
+	SELECT item_name
+	FROM orders
+	WHERE item_name LIKE '%bowl%');
+	
+-- same as 
+
+SELECT *
+FROM orders
+WHERE item_name LIKE '%bowl%';
+
+
+
+-- now let's use an aggregate
+SELECT *
+FROM orders
+
+
+SELECT *
+FROM orders
+WHERE quantity = (
+	SELECT MIN(quantity)
+	FROM orders
+	);
+	
+	
+	
+-- find the names of all current managers that are women
+
+USE employees;
+
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN(
+	SELECT emp_no
+	FROM dept_manager
+	WHERE to_date > NOW();
+	)
+AND gender LIKE '%f%';
+
+
+
+
+-- TEMP TABLES *********************************************
+
+
+
+USE test3;
+
+CREATE TEMPORARY TABLE current_salary AS (
+    SELECT employees.employees.first_name, 
+    employees.employees.last_name, 
+    employees.salaries.salary
+    FROM employees.salaries
+    JOIN employees.employees USING(emp_no)
+    WHERE to_date > curdate()
+);
+
+-- If we do 5% raises for all current employees
+-- what is the sum of those salaries
+
+-- UPDATE my_numbers SET n = n + 1 (where clause, if needed)
+
+UPDATE current_salary SET salary = salary + salary * .05;
+
+USE test3;
+SELECT sum(salary) FROM current_salary;
+
+SELECT sum(salary) FROM employees.salaries WHERE to_date > curdate();
+
+SELECT (SELECT sum(salary) FROM current_salary) - 
+(SELECT sum(salary) FROM employees.salaries
+WHERE to_date > curdate());
+
+SELECT 3 + 2;
+
+
+CREATE TEMPORARY TABLE fruits AS (
+    SELECT *
+    FROM fruits_db.fruits
+);
+
+
+UPDATE current_salary SET salary = 100;
+
+SELECT *
+FROM current_salary;
+
+
+-- Table EXERCISES review
+
+-- Exercise 1
+-- Using the example from the lesson, create a temporary table called employees_with_departments 
+-- Add a column named full_name to this table. It should be a VARCHAR whose length is the sum of the lengths of the first name and last name columns
+-- Update the table so that full name column contains the correct data
+-- Remove the first_name and last_name columns from the table.
+-- What is another way you could have ended up with this same table?
+
+-- step 1: create the query using the db_name.table_name syntax
+SELECT first_name, last_name, dept_name
+FROM employees.employees
+JOIN employees.dept_emp USING(emp_no)
+JOIN employees.departments USING(dept_no)
+WHERE to_date > curdate();
+
+-- use that query to make a temporary table
+USE test3;
+CREATE TEMPORARY TABLE employees_with_departments AS (
+    SELECT first_name, last_name, dept_name
+    FROM employees.employees
+    JOIN employees.dept_emp USING(emp_no)
+    JOIN employees.departments USING(dept_no)
+    WHERE to_date > curdate()
+);
+
+-- step 3
+ALTER TABLE employees_with_departments ADD full_name VARCHAR(30);
+
+-- step 4
+UPDATE employees_with_departments SET full_name = concat(first_name, ' ', last_name);
+
+-- step 5
+ALTER TABLE employees_with_departments DROP COLUMN first_name;
+ALTER TABLE employees_with_departments DROP COLUMN last_name;
+
+-- double check
+SELECT * FROM employees_with_departments;
+
+-- Another way to create the same result? Create the full_name in the original query
+
+
+
+
+
+-- Exercise 2
+-- Write the SQL necessary to transform the amount column such that it is stored as an integer representing the number of cents of the payment. For example, 1.99 should become 199.
+
+USE test3;
+
+-- clean up any old version of this table (only if it already exists)
+DROP TABLE IF EXISTS payments;
+
+CREATE TEMPORARY TABLE payments AS (
+    SELECT payment_id, customer_id, staff_id, rental_id, amount * 100 AS amount_in_pennies, payment_date, last_update
+    FROM sakila.payment
+);
+
+SELECT * FROM payments;
+DESCRIBE payments;
+
+ALTER TABLE payments MODIFY amount_in_pennies INT NOT NULL;
+
+DESCRIBE payments;
+
+
+
+
+
+-- Exercise 3
+-- Find out how the current average pay in each department compares to the overall, historical average pay. 
+-- In order to make the comparison easier, you should use the Z-score for salaries. 
+-- In terms of salary, what is the best department right now to work for? The worst?
+
+-- Historic average and standard deviation b/c the problem says "use historic average"
+-- 63,810 historic average salary
+-- 16,904 historic standard deviation
+
+-- Obtain the average histortic salary and the historic standard deviation of salary
+-- Write the numbers down:
+-- 63,810 historic average salary
+-- 16,904 historic standard deviation
+SELECT AVG(salary) AS avg_salary, std(salary) AS std_salary
+FROM employees.salaries ;
+
+CREATE TEMPORARY TABLE current_info AS (
+    SELECT dept_name, AVG(salary) AS department_current_average
+    FROM employees.salaries
+    JOIN employees.dept_emp USING(emp_no)
+    JOIN employees.departments USING(dept_no)
+    WHERE employees.dept_emp.to_date > curdate()
+    AND employees.salaries.to_date > curdate()
+    GROUP BY dept_name
+);
+
+-- Create columns to hold the average salary, std, and the zscore
+ALTER TABLE current_info ADD average FLOAT(10,2);
+ALTER TABLE current_info ADD standard_deviation FLOAT(10,2);
+ALTER TABLE current_info ADD zscore FLOAT(10,2);
+
+UPDATE current_info SET average = 63810;
+UPDATE current_info SET standard_deviation = 16904;
+
+-- z_score  = (avg(x) - x) / std(x) */
+UPDATE current_info 
+SET zscore = (department_current_average - historic_avg) / historic_std;
+
+SELECT * FROM current_info
+ORDER BY zscore DESC;
+
+
+
+
+-- Exercise 3 in a more programmatic way
+-- Historic average and standard deviation b/c the problem says "use historic average"
+-- 63,810 historic average salary
+-- 16,904 historic standard deviation
+USE florence08;
+
+
+CREATE TEMPORARY TABLE historic_aggregates AS (
+    SELECT AVG(salary) AS avg_salary, std(salary) AS std_salary
+    FROM employees.salaries 
+);
+
+CREATE TEMPORARY TABLE current_info AS (
+    SELECT dept_name, AVG(salary) AS department_current_average
+    FROM employees.salaries
+    JOIN employees.dept_emp USING(emp_no)
+    JOIN employees.departments USING(dept_no)
+    WHERE employees.dept_emp.to_date > curdate()
+    AND employees.salaries.to_date > curdate()
+    GROUP BY dept_name
+);
+
+SELECT * FROM current_info;
+
+ALTER TABLE current_info ADD historic_avg FLOAT(10,2);
+ALTER TABLE current_info ADD historic_std FLOAT(10,2);
+ALTER TABLE current_info ADD zscore FLOAT(10,2);
+
+UPDATE current_info SET historic_avg = (SELECT avg_salary FROM historic_aggregates);
+UPDATE current_info SET historic_std = (SELECT std_salary FROM historic_aggregates);
+
+SELECT * FROM current_info;
+
+UPDATE current_info 
+SET zscore = (department_current_average - historic_avg) / historic_std;
+
+SELECT * FROM current_info
+ORDER BY zscore DESC; 
