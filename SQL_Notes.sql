53f29f297c5e4c523f944f3d1954e6d96275c639
Desiree_McElroy <desireeashley90@gmail.com>
2021-04-05 09:32:15 -0700

    Update casese statement notes

--- SQL_Notes.sql
+++ SQL_Notes.sql
@@ -366,3 +366,199 @@ SET zscore = (department_current_average - historic_avg) / historic_std;
 
 SELECT * FROM current_info
 ORDER BY zscore DESC; 
+
+
+
+-- ***************
+-- CASE STATEMENTS
+
+-- If I'm only referencing one column and only testing for equality.
+
+SELECT
+    CASE COLUMN_NAME
+        WHEN condition_a THEN value_1
+        WHEN condition_b THEN value_2
+        ELSE value_3
+        END AS new_column_name
+FROM TABLE_NAME;
+
+/*
+CASE statement syntax. This allows me to reference different columns in my logic as well as use all of the conditional operators available to me in a WHERE Clause.
+*/
+SELECT
+    COLUMN_NAME,
+    CASE
+        WHEN COLUMN_NAME logic_1 THEN value1
+        WHEN COLUMN_NAME logic_2 THEN value2
+        WHEN COLUMN_NAME logic_3 THEN value3
+        ELSE catch_all_value
+        END AS new_column_name
+FROM TABLE_NAME;
+
+
+-- Choose the chipotle database
+USE chipotle;
+
+-- Check out my orders table.
+SELECT *
+FROM orders;
+
+-- Use a `CASE` statement to create bins called item_type using item names.
+SELECT 
+    item_name,
+    CASE
+        WHEN item_name LIKE '%chicken%' THEN 'Chicken Item'
+        WHEN item_name LIKE '%veggie%' THEN 'Veggie Item'
+        WHEN item_name LIKE '%beef%' THEN 'Beef Item'
+        WHEN item_name LIKE '%barbacoa%' 
+            OR item_name LIKE '%carnitas%' 
+            OR item_name LIKE '%steak%' THEN 'Specialty Item'       
+        WHEN item_name LIKE '%chips%' THEN 'Side'
+        ELSE 'Other'
+        END AS item_type
+FROM orders;
+
+
+-- How many different items do I have for each item type bin or category?
+SELECT 
+    CASE
+        WHEN item_name LIKE '%chicken%' THEN 'Chicken Item'
+        WHEN item_name LIKE '%veggie%' THEN 'Veggie Item'
+        WHEN item_name LIKE '%beef%' THEN 'Beef Item'
+        WHEN item_name LIKE '%barbacoa%' 
+            OR item_name LIKE '%carnitas%' 
+            OR item_name LIKE '%steak%' THEN 'Specialty Item'       
+        WHEN item_name LIKE '%chips%' THEN 'Side'
+        ELSE 'Other'
+        END AS item_type,
+    COUNT(*) count_of_records
+FROM orders
+GROUP BY item_type
+ORDER BY count_of_records DESC;
+
+
+-- Filter my return set to Specialty Items item types only and see which item in this category is most popular.
+SELECT 
+    item_name,
+    CASE
+        WHEN item_name LIKE '%chicken%' THEN 'Chicken Item'
+        WHEN item_name LIKE '%veggie%' THEN 'Veggie Item'
+        WHEN item_name LIKE '%beef%' THEN 'Beef Item'
+        WHEN item_name LIKE '%barbacoa%' 
+            OR item_name LIKE '%carnitas%' 
+            OR item_name LIKE '%steak%' THEN 'Specialty Item'
+        WHEN item_name LIKE '%chips%' THEN 'Side'
+        ELSE 'Other'
+        END AS item_type,
+    COUNT(*) AS count_of_records
+FROM orders
+GROUP BY item_type, item_name
+HAVING item_type = 'Specialty Item'
+ORDER BY count_of_records DESC;
+
+
+
+-- bucket data
+
+-- Create buckets for quantity to create a new categorical variable.
+SELECT
+    item_name,
+    CASE
+        WHEN quantity = 1 THEN 'single_item'
+        WHEN quantity BETWEEN 2 AND 5 THEN 'family_and_friends'
+        WHEN quantity BETWEEN 6 AND 9 THEN 'small_gathering'
+        WHEN quantity > 9 THEN 'party'
+        ELSE 'other'
+        END AS quant_cats
+FROM orders;
+
+
+-- Add a GROUP BY Clause to Zoom Out and take a look at my new categorical variables quant_cats
+SELECT
+    COUNT(*) AS count_of_records,
+    CASE
+        WHEN quantity = 1 THEN 'single_item'
+        WHEN quantity BETWEEN 2 AND 5 THEN 'family_and_friends'
+        WHEN quantity BETWEEN 6 AND 9 THEN 'small_gathering'
+        WHEN quantity > 9 THEN 'party'
+        ELSE 'other'
+        END AS quant_cats
+FROM orders
+GROUP BY quant_cats
+ORDER BY count_of_records DESC;
+
+
+
+-- Reference Multiple Columns
+
+-- Use mall_customers database.
+USE mall_customers;
+
+-- Check out the customers table.
+SELECT *
+FROM customers;
+
+
+
+-- Reference more than one column in CASE Statement logic.
+SELECT
+    gender,
+    age,
+    CASE
+        WHEN gender = 'Male' AND age < 20 THEN 'Teen Male'
+        WHEN gender = 'Male' AND age < 30 THEN 'Twenties Male'
+        WHEN gender = 'Male' AND age < 40 THEN 'Thirties Male'
+        WHEN gender = 'Male' AND age < 50 THEN 'Forties Male'
+        WHEN gender = 'Male' AND age < 60 THEN 'Fifties Male'
+        WHEN gender = 'Male' AND age < 70 THEN 'Sixties Male'
+        WHEN gender = 'Male' AND age >= 70 THEN 'Older Male'
+        WHEN gender = 'Female' AND age < 20 THEN 'Teen Female'
+        WHEN gender = 'Female' AND age < 30 THEN 'Twenties Female'
+        WHEN gender = 'Female' AND age < 40 THEN 'Thirties Female'
+        WHEN gender = 'Female' AND age < 50 THEN 'Forties Female'
+        WHEN gender = 'Female' AND age < 60 THEN 'Fifties Female'
+        WHEN gender = 'Female' AND age < 70 THEN 'Sixties Female'
+        WHEN gender = 'Female' AND age >= 70 THEN 'Older Female'
+        ELSE 'Other'
+        END AS gen_age_cat
+FROM customers;
+
+
+
+-- Zoom Out by adding a Group By Clause and a COUNT() function.
+SELECT
+    CASE
+        WHEN gender = 'Male' AND age < 20 THEN 'Teen Male'
+        WHEN gender = 'Male' AND age < 30 THEN 'Twenties Male'
+        WHEN gender = 'Male' AND age < 40 THEN 'Thirties Male'
+        WHEN gender = 'Male' AND age < 50 THEN 'Forties Male'
+        WHEN gender = 'Male' AND age < 60 THEN 'Fifties Male'
+        WHEN gender = 'Male' AND age < 70 THEN 'Sixties Male'
+        WHEN gender = 'Male' AND age >= 70 THEN 'Older Male'
+        WHEN gender = 'Female' AND age < 20 THEN 'Teen Female'
+        WHEN gender = 'Female' AND age < 30 THEN 'Twenties Female'
+        WHEN gender = 'Female' AND age < 40 THEN 'Thirties Female'
+        WHEN gender = 'Female' AND age < 50 THEN 'Forties Female'
+        WHEN gender = 'Female' AND age < 60 THEN 'Fifties Female'
+        WHEN gender = 'Female' AND age < 70 THEN 'Sixties Female'
+        WHEN gender = 'Female' AND age >= 70 THEN 'Older Female'
+        ELSE 'Other'
+        END AS gen_age_cat,
+    COUNT(*) AS count_of_customers
+FROM customers
+GROUP BY gen_age_cat
+ORDER BY count_of_customers DESC;
+
+
+-- Use the mall_customers database.
+USE mall_customers;
+
+-- Check out the customers table.
+SELECT *
+FROM customers;
+
+-- Use an IF Function to create a dummy variable for gender.
+SELECT
+    gender,
+    IF(gender = 'Female', TRUE, FALSE) AS is_female
+FROM customers;
